b84a72e3cb8cfd7b13c2b079ef87afd55a1939f1
SJ.Kang <kangsj0327@gmail.com>
2022-12-27 23:50:20 +0900

    1227

---
+++ S6_String/n1157.java
@@ -0,0 +1,37 @@
+package S6_String;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class n1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] alpha = new int[26];
+        String input = br.readLine();
+        int most = 0;
+        char mostIndex = 0;
+
+        for(int i = 0; i < input.length(); i++) {
+            if(i > 64 && i < 91) alpha[input.charAt(i)-65]++;
+            else if(i > 96 && i < 123) alpha[input.charAt(i)-97]++;
+        }
+
+        for(char i = 0; i < alpha.length; i++) {
+            if(most < alpha[i]) {
+                most = alpha[i];
+                mostIndex = i;
+            }
+            if(most == alpha[i]) {
+                mostIndex = 63;
+            }
+        }
+        if(mostIndex == 63) bw.write(mostIndex);
+        else bw.write(mostIndex+65);
+        br.close();
+        bw.close();
+    }
+}
